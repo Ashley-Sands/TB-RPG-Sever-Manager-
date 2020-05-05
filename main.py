32f0167c8e995077efbaa0cae2ad77f438aebff4
Ashley Sands <ashsands1@amstrike.com>
2020-05-05 21:29:35 +0100

    initial commit
    just messing at the mo :)
    
    but hoping to get some auto scaling working :)

---
+++ main.py
@@ -0,0 +1,61 @@
+import json, os, sys, subprocess
+
+impo = "import os\n"
+
+
+def get_command_data( command ):
+    command = "os.system( '" + command + "' )"
+
+    proc = subprocess.Popen( [ 'python', '-c', impo + command ], stdout=subprocess.PIPE )
+    json_str_ = ""
+
+    while True:
+        line = proc.stdout.readline().decode( "utf-8" )
+        if not line:
+            break
+        else:
+            json_str_ = json_str_ + line
+
+    proc.kill()
+
+    try:
+        return json.loads( json_str_ )
+    except:
+        return None
+
+
+server_type = "Standard_B1ms"  # (1 vcpus, 2 GiB memory)    # TODO: change to sys arg
+locations = [ "uksouth", "ukwest", "westeurope", "northeurope" ]
+location = None
+com = "az vm list-sizes --location {0} {1}"  # 0 = location, 1 = query to refine data
+query = '--query "[].{name:name}"'
+
+
+# query each of the location for an available server of server_type
+for loc in locations:
+    _com = com.format( loc, query )
+    print( _com )
+    data = get_command_data( _com )
+
+    if data is None:
+        print( "ERROR: converting json" )
+    elif len( data ) > 0:
+        for d in data:
+            if d[ "name" ] == server_type:
+                print( "Server is available at", loc )
+                location = loc
+                break
+
+        if location is None:
+            print( "Server is unavailable at", loc, ":(" )
+        else:
+            break
+    else:
+        print( "No Servers Available?? at", loc )
+
+# now we can spawn a new server at the location if it was available
+if location is None:
+    print("Error: can not create server, not available ant any location (", ' ,'.join(locations), ")")
+    exit()
+
+print("Spawning Server at", location, ":)")
