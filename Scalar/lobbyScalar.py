464a7967be74b1a520e339b47cc7cf6453fcc4ee
Ashley Sands <ashsands1@amstrike.com>
2020-05-17 23:05:22 +0100

    updated query

--- Scalar/lobbyScalar.py
+++ Scalar/lobbyScalar.py
@@ -3,8 +3,11 @@ import Scalar.baseScalar as baseScalar
 
 class LobbyScalar( baseScalar.BaseScalar ):
 
-     def init_commands( self ):
-         self.az_commands.add("list", "az container list "
-                                      "--resource-group rpg_network_we "
-                                      "--query '[].{id:id, ip:ipAddress.ip, image:containers[0].image, status:provisioningState}' "
-                                      "--output json")
+    def __init__( self, type_name, update_interval=60, max_instances=1 ):
+        super().__init__(type_name, update_interval, max_instances)
+
+    def init_commands( self ):
+        self.az_commands.add("list", "az container list "
+                                     "--resource-group rpg_network_we "
+                                     "--query {} "
+                                     "--output json")
