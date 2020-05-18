d226bb166be65b93315aaccfe187ac9a976df17d
Ashley Sands <ashsands1@amstrike.com>
2020-05-18 01:26:02 +0100

    fixed issue

--- Scalar/lobbyScalar.py
+++ Scalar/lobbyScalar.py
@@ -16,22 +16,22 @@ class LobbyScalar( baseScalar.BaseScalar ):
                                        "--ids {}"
                                        "--query {}")
 
-        self.az_commands.add("add", "az container create "
-                                    "-g rpg_network_we "
-                                    "--name {} "
-                                    "--cpu 0.25 "
-                                    "--memory 0.2 "
-                                    "--location westeurope "
-                                    "--port 8223 "
-                                    "--protocol TCP "
-                                    "--image {} "
-                                    "--registry-login-server index.docker.io "
-                                    "--registry-username gizzmoazure "
-                                    "--registry-password 'ghgi8754yt6iuregh95&85478^$%t6yg45rg45ut8rgu@_)7986utyhGBFv£!$^Y%ETHBGVFDhtuL<>&$^U%YGT$EVdcsx' "
-                                    "--restart-policy Never "
-                                    "--ip-address Private "
-                                    "--subnet /subscriptions/f3679ebd-da87-4256-9659-f5984d7c156f/resourceGroups/RPG_Network_WE/providers/Microsoft.Network/virtualNetworks/VNet-WE/subnets/container-net "
-                                    "--query {}")
+        self.az_commands.add("add", 'az container create '
+                                    '-g rpg_network_we '
+                                    '--name {} '
+                                    '--cpu 0.25 '
+                                    '--memory 0.2 '
+                                    '--location westeurope '
+                                    '--port 8223 '
+                                    '--protocol TCP '
+                                    '--image {} '
+                                    '--registry-login-server index.docker.io '
+                                    '--registry-username gizzmoazure '
+                                    '--registry-password "ghgi8754yt6iuregh95&85478^$%t6yg45rg45ut8rgu@_)7986utyhGBFv£!$^Y%ETHBGVFDhtuL<>&$^U%YGT$EVdcsx" '
+                                    '--restart-policy Never '
+                                    '--ip-address Private '
+                                    '--subnet /subscriptions/f3679ebd-da87-4256-9659-f5984d7c156f/resourceGroups/RPG_Network_WE/providers/Microsoft.Network/virtualNetworks/VNet-WE/subnets/container-net '
+                                    '--query {}')
 
     def request_new_instance( self ):
         """ request a new instances """
