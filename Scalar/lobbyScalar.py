da5df4f8c93182f13d7164801b02fa37893761a2
Ashley Sands <ashsands1@amstrike.com>
2020-05-18 02:02:55 +0100

    don't push your passwords to git kids
    glad i noticed that :D

--- Scalar/lobbyScalar.py
+++ Scalar/lobbyScalar.py
@@ -6,6 +6,9 @@ class LobbyScalar( baseScalar.BaseScalar ):
     def __init__( self, base_instance_name, type_name, update_interval=60, max_instances=1 ):
         super().__init__(base_instance_name, type_name, update_interval, max_instances)
 
+        self.docker_auth = self.az_commands.invoke("docker pass", background=False)[1]
+
+
     def init_commands( self ):
         self.az_commands.add("list", "az container list "
                                      "--resource-group rpg_network_we "
@@ -16,22 +19,24 @@ class LobbyScalar( baseScalar.BaseScalar ):
                                        "--ids {}"
                                        "--query {}")
 
-        self.az_commands.add("add", 'az container create '
-                                    '-g rpg_network_we '
-                                    '--name {} '
-                                    '--cpu 0.25 '
-                                    '--memory 0.2 '
-                                    '--location westeurope '
-                                    '--port 8223 '
-                                    '--protocol TCP '
-                                    '--image {} '
-                                    '--registry-login-server index.docker.io '
-                                    '--registry-username gizzmoazure '
-                                    '--registry-password "ghgi8754yt6iuregh95&85478^$%t6yg45rg45ut8rgu@_)7986utyhGBFv£!$^Y%ETHBGVFDhtuL<>&$^U%YGT$EVdcsx" '
-                                    '--restart-policy Never '
-                                    '--ip-address Private '
-                                    '--subnet /subscriptions/f3679ebd-da87-4256-9659-f5984d7c156f/resourceGroups/RPG_Network_WE/providers/Microsoft.Network/virtualNetworks/VNet-WE/subnets/container-net '
-                                    '--query {}')
+        self.az_commands.add("add", "az container create "
+                                    "-g rpg_network_we "
+                                    "--name {} "
+                                    "--cpu 0.25 "
+                                    "--memory 0.2 "
+                                    "--location westeurope "
+                                    "--port 8223 "
+                                    "--protocol TCP "
+                                    "--image {} "
+                                    "--registry-login-server index.docker.io "
+                                    "--registry-username gizzmoazure "
+                                    "--registry-password {} "
+                                    "--restart-policy Never "
+                                    "--ip-address Private "
+                                    "--subnet /subscriptions/f3679ebd-da87-4256-9659-f5984d7c156f/resourceGroups/RPG_Network_WE/providers/Microsoft.Network/virtualNetworks/VNet-WE/subnets/container-net "
+                                    "--query {}")
+
+        self.az_commands.add("docker pass", "az keyvault secret show --vault-name rpg-network-services --name docker --query 'value' ")
 
     def request_new_instance( self ):
         """ request a new instances """
@@ -40,6 +45,7 @@ class LobbyScalar( baseScalar.BaseScalar ):
                                               background=True,
                                               bg_callback=self.process_new_instance,
                                               name="{0}-{1}".format( self.instance_name, self.next_host_id ),
+                                              registrypassword=self.docker_auth,
                                               image=self.instance_type,
                                               query="'{id:id, ip:ipAddress.ip, type:containers[0].image, status:provisioningState}'")[0]
 
